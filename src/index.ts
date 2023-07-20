c8ff096e708933f8b3df2e8b2036fd9f20f631cc
JulenJR <julenjr@hotmail.com>
2023-07-20 12:01:27 +0200

    working socket chat

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import http from 'http';
+import { Server, Socket } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket: Socket) => {
+  console.log('A user connected');
+
+  socket.on('chat message', (data: { username: string; message: string }) => {
+    console.log(`[${data.username}]: ${data.message}`);
+    io.emit('chat message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
